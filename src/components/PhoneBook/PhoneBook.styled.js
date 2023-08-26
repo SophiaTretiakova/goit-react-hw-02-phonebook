3c79b01119777b29d0219f45c07e44a02f223c52
SophiaTretiakova <122561236+SophiaTretiakova@users.noreply.github.com>
2023-08-26 21:57:15 +0300

    improve deploy error

--- src/components/PhoneBook/PhoneBook.styled.js
+++ src/components/PhoneBook/PhoneBook.styled.js
@@ -1,5 +1,5 @@
 import { styled } from 'styled-components';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Form } from 'formik';
 
 export const StyledForm = styled(Form)`
   display: flex;
